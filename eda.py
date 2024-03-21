60c78e1071d11a8942e077503da425065a4dba39
danieltummler <115984509+danielwtummler@users.noreply.github.com>
2024-03-21 19:01:51 +0100

    cambios

--- eda.py
+++ eda.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 from plotly.subplots import make_subplots
 from modules.ml_func import *    
